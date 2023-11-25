f6e438f934dd3e8633edfae7d71ec91c876b1402
FarooQ <74699135+FarooQ-2113@users.noreply.github.com>
2023-11-25 11:54:21 +0530

    Update SuperMarket.cpp

--- SuperMarket.cpp
+++ SuperMarket.cpp
@@ -4,34 +4,27 @@ using namespace std;
 int main(){
     int n,m；
     cin>>n>>m；
-    // Telegram - @PLACEMENTLELO
     vector<vector<int>> cust;
     for(int i=0; i<n; i++){
         int q,p;
         cin>>q>>p;
         cust.push_back({p,q});
     }
-    // Telegram - @PLACEMENTLELO
     vector<vector<int>> rice;
     for(int i=0; i<m; i++){
         int q,p;
         cin>>q>>p;
         rice.push_back({p,q});
     }
-    // Telegram - @PLACEMENTLELO
     sort(cust.begin(), cust.end());
     sort(rice.begin(), rice.end());
-    // Telegram - @PLACEMENTLELO
     vector<int> placementlelo(m,0);
-    // Telegram - @PLACEMENTLELO
     int ans = 0;
-    // Telegram - @PLACEMENTLELO
     for(int i=0; i<n; i++){
         int quan = -1;
         int index = -1;
         for(int j=0; j<m; j++){
             if(!placementlelo[j]){
-                // Telegram - @PLACEMENTLELO
                 if(rice[j][0] > cust[i][0]) break;
                 
                 if(rice[j][1] > cust[i][1]){
@@ -48,13 +41,11 @@ int main(){
                 }
             }
         }
-        // Telegram - @PLACEMENTLELO
         if(index != -1){
             placementlelo[index] = 1;
             ans++;
         }
     }
-    // Telegram - @PLACEMENTLELO
     cout<<ans;
 }
     
